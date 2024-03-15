091bf889833d30082d9fa263c55f65417b9f7c5f
Elias Häußler <e.haeussler@familie-redlich.de>
2024-03-15 08:17:55 +0100

    [TASK] Reflect supported TYPO3 versions

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'coding. powerful. systems. CPS GmbH',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
+            'typo3' => '11.5.0-13.0.99',
             'php' => '8.1.0-8.3.99',
         ],
         'suggests' => [
